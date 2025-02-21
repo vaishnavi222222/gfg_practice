3bbe04aaccf8cc557db56af9c2f80b5c28f09dfc
vaishnavi222222 <151459102+vaishnavi222222@users.noreply.github.com>
2025-02-21 11:43:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Data Type/data-type.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            int ans = obj.dataTypeSize(s);
+            String Character = "Character";
+            if (Character.equals(s) && ans == 2) {
+                ans = 1;
+            }
+            System.out.println(ans);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int dataTypeSize(String str) {
+        if(str.equals("Character"))return 1;
+        else if(str.equals("Integer") || str.equals("Float")) return 4;
+        else  return 8;
+    }
+}
